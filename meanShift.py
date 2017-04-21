cb6a49b582bf1a09618e7661a696bfadd1d6d425
Yusuf Yiğit Pilavcı <yigit.pilavci01@gmail.com>
2017-04-22 02:28:26 +0300

    fcommit

--- meanShift.py
+++ meanShift.py
@@ -21,7 +21,7 @@ while 1:
 		roi_hist = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180])
 		cv2.normalize(roi_hist,roi_hist,0,255,cv2.NORM_MINMAX)
 	
-		c = cv2.rectangle(frame, (r,c), (r+w,c+h), 255,2)
+		c = cv2.rectangle(frame, (r,c), (r+w,c+h), (0,0,255),15)
 
 		cv2.imshow('img2',frame)
 
